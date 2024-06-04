4eb67b9c0d0fcd58813f4fac0a6061ba73ded791
RitikPatel00 <patelritik0022@gmail.com>
2024-06-04 17:52:01 +0530

    spring boot app

---
+++ spring-boot-app/spring-boot-app/src/main/java/com/wipro/controller/UserController.java
@@ -0,0 +1,87 @@
+package com.wipro.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wipro.model.User;
+import com.wipro.service.UserService;
+
+@RestController
+@RequestMapping("/uc")
+public class UserController {
+	@Autowired
+	private UserService userService;
+	
+	
+	//http://localhost:9090/uc/users/user1
+	@RequestMapping("/users/{username}")
+	public User getUserByName( @PathVariable(value = "username") String username) throws Exception{
+		try {
+			User user = userService.getUserByName(username);
+			if(user ==null ) {
+				throw new Exception("Invalid Username");
+			}
+			
+			return user;
+		}catch(Exception e) {
+			throw e;
+		}
+	}
+	
+
+	//http://localhost:9090/uc/users
+
+	@GetMapping("/users")
+	public List<User> getAllUsers() throws Exception{
+		try {
+			List<User> userList = userService.getAllUsers();
+			if(userList.size() == 0 ) {
+				throw new Exception("User List is empty");
+			}
+			
+			return userList;
+		}catch(Exception e) {
+			throw e;
+		}
+	}
+	
+	//http://localhost:9090/uc/users
+
+	@PostMapping("/users")
+	public User addUser(@RequestBody User user) {
+		User newUser = userService.addUser(user);
+		return newUser;
+	}
+	
+	
+
+
+	@PutMapping("/users")
+	public User updateUserPassword(@RequestBody User user) {
+		return userService.updateUserPassword(user);
+	}
+
+
+
+
+	//http://localhost:9090/uc/users?username=user1
+	
+	@DeleteMapping("/users")
+	public String deleteUser(@RequestParam(value = "username") String username) {
+		return userService.deleteUser(username);
+	}
+
+	
+}
+
+
